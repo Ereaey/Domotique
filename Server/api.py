401f4645a0cf652f2a2fa4eb2f603bf4ba226479
Ereaey <anthony.arthus@gmail.com>
2016-01-24 18:32:37 +0100

    Mise a jour API : log

--- Server/api.py
+++ Server/api.py
@@ -23,10 +23,10 @@ def rooms():
 	c = coon.cursor()
 	c.execute("""SELECT * FROM rooms""")
 	rows = c.fetchall()
-	data = ""
+	data = []
 	for row in rows:
-		data += json.dumps({"id" : row[0], "name": row[1]})
-	return data
+		data.append({"id" : row[0], "name": row[1]})
+	return json.dumps({"rooms" : data})
 
 @app.route('/equipments_type')
 def types():
@@ -34,10 +34,10 @@ def types():
 	c = coon.cursor()
 	c.execute("""SELECT * FROM equipments_type""")
 	rows = c.fetchall()
-	data = ""
+	data = []
 	for row in rows:
-		data += json.dumps({"id" : row[0], "name": row[1], "path_image": row[2]})
-	return data
+		data.append({"id" : row[0], "name": row[1], "path_image": row[2]})
+	return json.dumps({"equipments_type" : data})
 
 @app.route('/room/:id')
 def room(id):
@@ -45,10 +45,21 @@ def room(id):
 	c = coon.cursor()
 	c.execute("""SELECT * FROM equipments WHERE id_room=?""", (id, ))
 	rows = c.fetchall()
-	data = ""
+	data = []
 	for row in rows:
-		data += json.dumps({"id" : row[0], "name": row[4]})
-	return data
+		data.append({"id" : row[0], "name": row[4]})
+	return json.dumps({"room" : data})
+
+@app.route('/equipments')
+def equipments():
+	coon = sqlite3.connect('domotique.sqlite')
+	c = coon.cursor()
+	c.execute("""SELECT * FROM equipments""")
+	rows = c.fetchall()
+	data = []
+	for row in rows:
+		data.append({"id" : row[0], "name": row[4]})
+	return json.dumps({"equipments" : data})
 
 @app.route('/addEquipment/:id_type/:id_protocol/:id_room/:name/:data1/:data2/:data3/:unit')
 def addEquipment(id_type, id_protocol, id_room, name, data1, data2, data3, unit):
@@ -64,10 +75,10 @@ def protocoles():
 	c = coon.cursor()
 	c.execute("""SELECT * FROM protocols""")
 	rows = c.fetchall()
-	data = ""
+	data = []
 	for row in rows:
-		data += json.dumps({"id" : row[0], "name": row[1], "description" : row[2]})
-	return data
+		data.append({"id" : row[0], "name": row[1], "description" : row[2]})
+	return json.dumps({"protocols" : data})
 
 @app.route('/')
 def api():
@@ -82,7 +93,7 @@ def api():
 	data += "/stats<br />"
 	return data
 
-@app.route('/log')
+@app.route('/log_udp')
 def log():
 	file = open("log.txt", "r")
 	data = ""
@@ -91,6 +102,17 @@ def log():
 		data += '<br />'
 	file.close()
 	return data
+
+@app.route('/log_server')
+def log():
+	file = open("log_s.txt", "r")
+	data = ""
+	for line in file:
+		data += line
+		data += '<br />'
+	file.close()
+	return data
+
 
 @app.route('/stats')
 def stats():
@@ -104,7 +126,6 @@ def stats():
 	data += '<br />'
 	data += "CPU : "
 	data += str(psutil.cpu_percent(interval=1, percpu=True))
-	print(data)
 	return data
 
-run(app, host='localhost', port=8080, reloader=True)
+run(app, host='localhost', port=8080)
