fda469e267f064c7a44fb5e274e637c76e99d7c2
Ereaey <anthony.arthus@gmail.com>
2016-01-25 22:30:23 +0100

    Ajout systeme demarrage

---
+++ Server/run.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+import sys
+import time
+
+print "Demarrage"
+main = subprocess.Popen(['python', 'main.py', '>>', 'log_s.txt'])
+api = subprocess.Popen(['python3', 'api.py'])
+modules = subprocess.Popen(['python', '../Module/main.py', '>>', 'log_mod.txt'])
+
+while(1):
+	try:
+		time.sleep(60)
+		print "Module en cours d'execution"
+	except KeyboardInterrupt:
+		print "Fermeture"
+		main.terminate()
+		api.terminate()
+		modules.terminate()
+		time.sleep(2)
+		sys.exit(0)
