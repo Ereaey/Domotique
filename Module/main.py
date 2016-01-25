4991c599485b0e02cc27a932bab8c47dff166796
Ereaey <anthony.arthus@gmail.com>
2016-01-25 18:49:57 +0100

    Ajout flush systeme log

--- Module/main.py
+++ Module/main.py
@@ -7,15 +7,18 @@ print "Module Ping, Mail, Clock"
 ping = subprocess.Popen(['python', 'xpl_ping.py'])
 mail = subprocess.Popen(['python', 'xpl_mail.py'])
 clock = subprocess.Popen(['python', 'xpl_clock.py'])
+sys.stdout.flush()
 
 while(1):
 	try:
 		time.sleep(60)
 		print "Module en cours d'execution"
+		sys.stdout.flush()
 	except KeyboardInterrupt:
 		print "Fermeture des modules"
 		ping.terminate()
 		mail.terminate()
 		clock.terminate()
 		time.sleep(2)
+		sys.stdout.flush()
 		sys.exit(0)
